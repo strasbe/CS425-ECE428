660c950e74e69e8cac9d9beb7c83ba53de5c205b
Kevin Strasberg <strasbe1@illinois.edu>
2013-09-14 19:12:41 -0500

    House Keeping stuff

--- MP1/runGrep.js
+++ MP1/runGrep.js
@@ -3,7 +3,6 @@ var logFile = 'machine.1.log';
 var runGrep = function (cmd, cb) {
   cb = cb || function () { }
   var spawn = require('child_process').spawn;
-  var colors = require('colors');
   grep  = spawn('grep', ['-e', cmd.toString('utf-8'), logFile]);
 
   grep.stdout.on('data', function (data) {
